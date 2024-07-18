7e4e6e277fff563296981afb1b65b0a0ad21a228
jsaldien <jelle.saldien@ugent.be>
2024-07-18 11:32:38 +0200

    category file added to experience economy chapter

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,14 +46,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/UA-Product-Development/EAP-wiki/',
+            'https://github.com/UA-Product-Development/EAP-wiki/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/UA-Product-Development/EAP-wiki/',
+            'https://github.com/UA-Product-Development/EAP-wiki/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
